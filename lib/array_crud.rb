9485e433435e7a0f5d574a88d76c82dc97c39fb3
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-15 19:36:08 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,48 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
-end
+  ["mama", "mommy", "daughter", "son"]
+  end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["dogs", "cats","birds"]
+  array << "arrays!"
+  p array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["dogs", "cats","birds"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["dogs", "cats","birds", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow","dogs", "cats","birds", "arrays!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow","dogs","cats","birds","am","arrays!"]
+  array[4]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow","dogs","cats","birds","am","arrays!"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow","dogs","cats","birds","am","arrays!"]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+array = ["wow","dogs","cats","totally","birds","am","arrays!"]
+array[3]
 end
